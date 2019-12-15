2fca64f6d4fe3aeac3cb3c39a7eac644e042babb
klau2000 <klau00@stuy.edu>
2019-12-15 13:44:37 -0500

    all good

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main (int argc, char *argv[]) {
         fgets(input, sizeof input, stdin);
         input[strlen(input) - 1] = '\0';
         if(execute(input)){
-            return 0;
+            break;
         }
     }
     return 0;
